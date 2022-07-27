ad04354ba5124d1c49510ff422e1d65ce9992335
Matias Aristimuno <mattaristi@MacBook-Air-de-Ariel.local>
2022-07-27 23:48:25 +0100

    error

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,11 @@
 import './Navbar.css'
-import logoRaw from 'https://drive.google.com/file/d/1n4YztrfMzuBrWouhbn_pVL9aZBfmd3-t/view?usp=sharing'
+import logoRaw from './imagenesraw/logoblack.jpg'
 
 const Navbar = () => {
     return (
        <nav>
         <div>
-            <img src={logoRaw}></img>
+            <img src={logoRaw} alt={"logoRaw"}></img>
         </div>
         <div>
             <button className="btn-loguin">loguin</button>
