39312aa8b76d02734391dd572f2c26685b74c9dd
Matias Aristimuno <mattaristi@MacBook-Air-de-Ariel.local>
2022-08-06 03:10:17 +0100

    entrega contador

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import ItemListContainer from './components/ItemListContainer/ItemListContainer';
+import ItemCount from './components/ItemCount/ItemCount';
 function App() {
+  
+  const addQuantity = (quantity,stock) =>{
+    if (quantity<=stock)
+     console.log('Total',quantity);
+    
+  }
+  
+  
+  
   return (
     <div className="App">
       <header className="App-header">
         <Navbar/>
         <ItemListContainer greeting="Raw Bikes"/>
-        
-
+        <ItemCount stock={10} initial={0} onAdd={addQuantity}/>
       </header>
     </div>
   )
---
+++ src/components/ItemCount/ItemCount.js
@@ -0,0 +1,27 @@
+import './ItemCount.css'
+import {useState} from "react";
+
+const ItemCount = ({stock,initial,onAdd}) => {
+    const[count, setCount]= useState(0)
+    const decrement = () => {
+        if (count>initial)
+        setCount(count - 1)
+    }
+ 
+    const increment = () => {
+        if (count< stock)
+        setCount(count +1)
+    }
+    return(
+        <div>
+        <div className='counter'>
+            <button className="btn-count" onClick={decrement}>-</button>
+            <p className='qantity'>{count}</p>
+            <button className="btn-count" onClick={increment}>+</button>
+        </div>
+            <button onClick={() => onAdd (count, stock)} className="btn-count">Add to cart</button>
+        </div>
+    )
+}
+
+export default ItemCount;
